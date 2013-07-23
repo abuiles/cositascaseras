4eb26726a3d1c88b23a8689b8687c2fdad5dd501
Adolfo Builes <builes.adolfo@gmail.com>
2013-07-22 22:51:39 -0300

    Fix sitemaps.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.cositascaseras.com"
 
-SitemapGenerator::Sitemap.sitemaps_host = "https://cositas-caseras.s3.amazonaws.com/sitemap-generator/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://cositas-caseras.s3.amazonaws.com/sitemaps/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
